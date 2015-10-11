5937e9d58d69f41b7f70e8f6a79f48f4fe3f6cd3
analca3 <analca3@gmail.com>
2015-10-11 16:03:14 +0200

    Cambiado prueba por práctica

--- Prácticas/src/practica1.cpp
+++ Prácticas/src/practica1.cpp
@@ -2,10 +2,6 @@
 #include <iostream>
 
 
-
-using namespace std;
-using namespace cv;
-
 int main(int argc, char const *argv[]) {
   Image prueba(string("../Imágenes/lena.jpg"),CV_LOAD_IMAGE_COLOR);
   prueba.paint();
