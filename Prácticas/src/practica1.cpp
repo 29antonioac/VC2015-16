5fa861cde5a534643d66b7a9691d930f99982294
analca3 <analca3@gmail.com>
2015-11-06 21:53:59 +0100

    Añadido practica 2 y modificado make para tal

---
+++ Prácticas/src/practica1.cpp
@@ -0,0 +1,16 @@
+#include "../inc/image.hpp"
+#include "../inc/utils.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+
+int main(int argc, char const *argv[]) {
+
+
+  waitKey(0);
+  destroyAllWindows();
+
+  return 0;
+}
