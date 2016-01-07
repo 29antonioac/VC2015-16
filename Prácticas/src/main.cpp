c5eae71ff4564c18303684f443779c0d378840bd
analca3 <analca3@gmail.com>
2016-01-07 17:00:58 +0100

    Adding excersice 3

--- Prácticas/src/main.cpp
+++ Prácticas/src/main.cpp
@@ -11,6 +11,7 @@ using namespace std;
 using namespace cv;
 
 int main(int argc, char const *argv[]) {
+  RNG rng;
   theRNG().state = clock();
   vector<Point3f> worldPoints;
   vector<Point2f> projectedPoints;
@@ -59,7 +60,7 @@ int main(int argc, char const *argv[]) {
 
   /* Exercise 2 */
 
-
+/*
   const int CHESS_IMAGES = 25;
   bool valid;
   vector<Point2f> corners;
@@ -89,6 +90,7 @@ int main(int argc, char const *argv[]) {
     waitKey();
     destroyAllWindows();
   }
+
 
   vector< vector<Point3f> > objectPoints;
   vector<Point3f> points;
@@ -121,6 +123,87 @@ int main(int argc, char const *argv[]) {
 	double error = calibrateCamera(objectPoints, imagePoints, imageSize, cameraMatrix, distCoeffs, rotationVectors, translationVectors, flags);
 
   cout << "Error calibrando = " << error << endl;
+  */
+
+  /* Excercise 3 */
+
+  int Threshl=65;
+  int Octaves=3;
+  float PatternScales=1.0f;
+
+  vector<Mat> vmort;
+  vector<KeyPoint> keypoints[2];
+  Mat descriptors[2];
+
+  for (int i = 1; i <= 2; i++)
+  {
+    vmort.push_back(imread("imagenes/Vmort" + to_string(i) + ".pgm",CV_LOAD_IMAGE_COLOR));
+  }
+
+  // Detect and compute descriptors
+  Ptr<BRISK> ptrBrisk = BRISK::create(Threshl,Octaves,PatternScales);
+
+  ptrBrisk->detect(vmort[0], keypoints[0]);
+  ptrBrisk->compute(vmort[0], keypoints[0],descriptors[0]);
+
+  ptrBrisk->detect(vmort[1], keypoints[1]);
+  ptrBrisk->compute(vmort[1], keypoints[1],descriptors[1]);
+
+  BFMatcher matcher(NORM_HAMMING, true);
+
+  vector<DMatch> matches;
+
+  // Match!
+  vector<Point2f> corresp[2];
+  matcher.match(descriptors[0], descriptors[1], matches);
+
+  // Get correspondence points
+  for (int i = 0; i < matches.size(); i++)
+  {
+		corresp[0].push_back(keypoints[0][ matches[i].queryIdx ].pt);
+		corresp[1].push_back(keypoints[1][ matches[i].trainIdx ].pt);
+	}
+
+  // Compute fundamental matrix
+  Mat F = cv::findFundamentalMat(corresp[0], corresp[1], CV_FM_RANSAC, 0.001);
+  cout << F << endl;
+
+  // Compute epilines
+  vector<Vec3f> epilines[2];
+
+  computeCorrespondEpilines(corresp[0], 1, F, epilines[1]);
+  computeCorrespondEpilines(corresp[1], 2, F, epilines[0]);
+
+  double distance = 0.0;
+  int epilineIndex;
+
+  for (epilineIndex = 0; epilineIndex < epilines[0].size() && epilineIndex < 200; epilineIndex++)
+  {
+    Scalar color = Scalar(rng.uniform(0,255), rng.uniform(0, 255), rng.uniform(0, 255));
+    for (int image = 0; image < 2; image++)
+    {
+  		Vec3f epiline = epilines[image].at(epilineIndex);
+  		Point p = Point(0, -epiline[2] / epiline[1]);
+  		Point q = Point(vmort[image].cols, (-epiline[2] - epiline[0] * vmort[image].cols) / epiline[1]);
+
+      distance += fabs(epiline[0] * corresp[image][epilineIndex].x + epiline[1] * corresp[image][epilineIndex].y + epiline[2]) / sqrt(epiline[0]*epiline[0] + epiline[1]*epiline[1]);
+
+  		line(vmort[image], p, q, color);
+  		circle(vmort[image], corresp[image][epilineIndex], 5, color);
+  	}
+  }
+
+  // Compute average of distance between correspondences and epilines
+  distance /= 2 * (epilineIndex - 1);
+
+  cout << "Average distance = " << distance << endl;
+
+  imshow("Vmort 1", vmort[0]);
+  imshow("Vmort 2", vmort[1]);
+
+  waitKey();
+  destroyAllWindows();
+
 
   return 0;
 }
